c99b4c05d7ad1df51b8ad218363020f772d295b1
ilhanyumer <ilhan@ilhan.name>
2022-08-17 11:20:28 +0300

    Move import place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store';
-import { counterReducer } from './counter.reducer';
 
 import { AppComponent } from './app.component';
 
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
+
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoreModule.forRoot({count: counterReducer})],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
